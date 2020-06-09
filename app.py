11d5e159ee23df3f26f23eaa1cd3bd065a41a498
Akash Kumar <akashkrcse1@gmail.com>
2020-06-09 14:32:58 +0530

    Added Exception Handling

--- app.py
+++ app.py
@@ -2,28 +2,31 @@
 
 # function to take inputs
 def takeinput():
-    m = int(input("Enter number of players on each side: "))
-    player_name_with_score = {}
-    print("Enter " + str(m * m) + " players names with their score")
-    count = 1
-    while True:
-        if (count > (m * m)):
-            break
+    try:
+        m = int(input("Enter number of players on each side: "))
+        player_name_with_score = {}
+        print("Enter " + str(m * m) + " players names with their score")
+        count = 1
+        while True:
+            if (count > (m * m)):
+                break
 
-        name = input("Name of player " + str(count) + ": ")
-        score = int(input("Score: "))
-        if (name == "" and score == ""):
-            break
-        elif (name == "" or score == ""):
-            print("Enter all player values to proceed.")
-            break
-
-        player_name_with_score[name]=score
-        count += 1
-    return m, m * m,player_name_with_score
+            name = input("Name of player " + str(count) + ": ")
+            score = int(input("Score: "))
+            if (name == ""):
+                print("Enter all player name to proceed.")
+                break
+            player_name_with_score[name]=score
+            count += 1
+        return m, m * m,player_name_with_score
+    except Exception:
+        print("Error encountered while taking values")
 
 # driver program
 if __name__ == "__main__":
-    players_on_each_side, total_no_of_players, player_name_with_score = takeinput()
-    print(players_on_each_side, total_no_of_players)
-    print(player_name_with_score)
+    try:
+        players_on_each_side, total_no_of_players, player_name_with_score = takeinput()
+        print(players_on_each_side, total_no_of_players)
+        print(player_name_with_score)
+    except Exception:
+        print("Error encountered in driver program")
